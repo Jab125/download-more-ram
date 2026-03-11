80a404a2b360b428470b54241ab2e18cd3a18e08
Jab125 <67534807+Jab125@users.noreply.github.com>
2026-03-11 21:58:59 +1100

    one down

--- src/main/java/dev/jab125/drm/mixin/LogoRendererMixin.java
+++ src/main/java/dev/jab125/drm/mixin/LogoRendererMixin.java
@@ -2,7 +2,7 @@ package dev.jab125.drm.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.LogoRenderer;
 import net.minecraft.util.ARGB;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LogoRenderer.class)
 public class LogoRendererMixin {
-	@Inject(method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V", at = @At("RETURN"))
-	void render(final GuiGraphics graphics, final int width, final float alpha, final int heightOffset, CallbackInfo ci) {
+	@Inject(method = "extractRenderState(Lnet/minecraft/client/gui/GuiGraphicsExtractor;IFI)V", at = @At("RETURN"))
+	void render(final GuiGraphicsExtractor graphics, final int width, final float alpha, final int heightOffset, CallbackInfo ci) {
 		int logoHeight = 44 + 14;
 		int logoWidth = LogoRenderer.LOGO_WIDTH;
 		Font font = Minecraft.getInstance().font;
@@ -21,7 +21,7 @@ public class LogoRendererMixin {
 		graphics.pose().pushMatrix();
 		graphics.pose().translate(width / 2 - 128, heightOffset);
 		graphics.pose().scale((float) logoWidth / width1, (float) logoHeight / font.lineHeight);
-		graphics.drawString(font, "67", 0, 0, ARGB.color(alpha, 0xffffff));
+		graphics.text(font, "67", 0, 0, ARGB.color(alpha, 0xffffff));
 		graphics.pose().popMatrix();
 	}
 }
